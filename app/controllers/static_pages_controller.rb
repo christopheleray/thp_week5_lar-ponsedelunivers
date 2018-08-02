4c9889d2c018addfddfd5f7a4ae3fd8b14e9d0f0
christopheleray <christophe.leray1@gmail.com>
2018-08-02 18:55:16 +0200

    modif secret pages, application cntroller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,20 @@
 class StaticPagesController < ApplicationController
    before_action :require_login, only: [:secret_page]
+  
   def home
+    
   end
 
   def secret_page
   end
+
+   private 
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to login_path # halts request cycle
+    end
+  end
+
 end
