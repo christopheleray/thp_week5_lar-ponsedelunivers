d5b82ee12b6d7e1e02785aba42d8463414a581e7
breizzh <remisamsoninvestisseur@gmail.com>
2018-08-02 14:15:42 +0200

    password confirmation bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,41 @@
 class UsersController < ApplicationController
-
   def new
+  	puts "=================new=============================="
     @user = User.new
+    puts "---------------------end--------------------------"
   end
 
   def create
-    @user = User.create
+  	puts "====================create========================"
+    user = User.create(user_params)
+    puts params.inspect
+    redirect_to home_path
+    puts "---------------------end--------------------------"
+  end
+
+  def show
+    puts "--------------------show--------------------------"
+    puts "---------------------end--------------------------"
+  end
+
+  def edit
+  	puts "====================edit=========================="
+  	puts "---------------------end--------------------------"
   end
 
   def delete
+  	puts "====================delete========================"
+  	puts "---------------------end--------------------------"
   end
 
+  def update
+  	puts "=====================update======================="
+  	puts "---------------------end--------------------------"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
