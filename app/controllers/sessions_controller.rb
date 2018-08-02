2f681e104ec98fd104eec22107256c478bc959a5
christopheleray <christophe.leray1@gmail.com>
2018-08-02 19:27:11 +0200

    remember me cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,21 +5,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	puts "============create session================"
-  	user = User.find_by(email: params[:session][:email])
-  	if user && user.authenticate(params[:session][:password])
-  		log_in user
-  		redirect_to user
-  	else
-  		flash.now[:danger] = "Invalid email or/and password !"
-  		render 'new'
-  	end
-  	puts "------------------------------------------"
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      remember user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
   	puts "============destroy session==============="
-    log_out
+    log_out if logged_in?
     redirect_to home_path
   	puts "------------------------------------------"
   end
